2ef6739838c2294c61a5ae0a356b39a8724764fd
Kim-YoungKwon <rladudrnjs61@naver.com>
2020-04-29 13:04:09 +0900

    asdasd

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -32,26 +32,4 @@ $(function(){
             $(this).parents('.modal').addClass('hidden');
         });
     });
-<<<<<<< HEAD
-
-    $('input.number-only').on('keyup', function(){
-        $(this).val($(this).val().replace(/[^0-9]/g,""));
-    });
-
-    /* 후원 상세페이지 */
-    $('.campaign-tab-nav .tab-item a').on('click', function(e){
-        e.preventDefault();
-        $('.campaign-tab-nav .tab-item').removeClass('active');
-        $('.campaign-tab-content .tab-content-item').removeClass('active');
-
-        $(this).parent().addClass('active');
-        $($(this).attr('href')).addClass('active');
-    });
-
-    $('a.qna-title').on('click', function(e){
-        e.preventDefault();
-        $($(this).attr('href')).toggleClass('hidden');
-    });
-=======
->>>>>>> project_J
 });
