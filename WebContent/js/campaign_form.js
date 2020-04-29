e0b82bcb7e250a988a95c76fbfe4602f1a48b523
jungjinbeom <insunok0715@naver.com>
2020-04-29 16:04:46 +0900

    :asd

--- WebContent/js/campaign_form.js
+++ WebContent/js/campaign_form.js
@@ -18,25 +18,13 @@ $(function(){
 		$('.del_year').append('<option>'+ nowYear++ +'</option>');
 	}
 
-<<<<<<< HEAD
-=======
 	$('.reward-fieldset').each(function(){
 		console.log($(this).index());
 	});
->>>>>>> project_J
 
 	//숫자포맷 세팅
 	$('.min_money, .max_money, .goal').number(true, 0);
 
-<<<<<<< HEAD
-
-	$(document).on('click', '.add-reward-row', function(e){
-		e.preventDefault();
-		var fieldset = '<div id="reward-fieldset-1"data-number="1"class="reward-fieldset bg-gray-lightest px-4 mb-4"><div class="mb-4 py-4 border-b border-gray"><h3 id="reward-title-1"class="reward-title">리워드<span class="reward-number">1</span></h3></div><div class="mb-4"><label for="reward_name"class="inline-block my-4">리워드명</label><input type="text"name="reward[reward_name_1]"id="reward_name_1"data-name="reward_name"class="appearance-none border border-gray rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"></div><div class="mb-4"><label for="reward_desc"class="inline-block my-4">리워드설명</label><textarea name="reward[reward_desc_1]"id="reward_desc_1"data-name="reward_desc"placeholder="리워드에 대한 간략한 설명"class="appearance-none border border-gray rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline h-24"></textarea></div><div class="mb-4 flex"><div class="w-1/3 pr-8"><label for="reward_limit"class="inline-block my-4">리워드수량</label><input type="number"name="reward[reward_limit_1]"data-name="reward_limit"min="0"id="reward_limit_1"class="number-only appearance-none border border-gray rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"></div><div class="w-1/3 pr-8"><label for="del_fee"class="inline-block my-4">배송비</label><div class="relative"><input type="text"name="reward[del_fee_1]"id="del_fee_1"data-name="del_fee"class="appearance-none border border-gray rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"><div class="absolute inset-y-0 right-0 py-2 px-3 text-gray-darkest">원</div></div></div></div><div class="mb-4"><label for="del_date"class="inline-block my-4 w-full">배송일정</label><div class="flex"><div class="w-1/3 flex"><div class="inline-block relative w-1/2 mr-2"><!--select--><select name="reward[del_year_1]"id="del_year_1"data-name="del_year"class="del_year block appearance-none w-full bg-white border border-gray hover:border-gray-dark px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"><option>년</option></select><div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2"><i class="xi-angle-down-min"></i></div><!--select--></div><div class="inline-block relative w-1/2 mr-2"><!--select--><select name="reward[del_month_1]"id="del_month_1"data-name="del_month"class="block appearance-none w-full bg-white border border-gray hover:border-gray-dark px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"><option>월</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select><div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2"><i class="xi-angle-down-min"></i></div><!--select--></div></div><div class="w-2/3"><input type="text"name="reward[del_date_detail_1]"id="del_date_detail_1"data-name="del_date_detail"placeholder="상세일정 ex) 21~말일 배송 예정입니다."class="datepicker-ym appearance-none border border-gray rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"></div></div></div><div class="reward-ctrl-btn py-4 flex justify-end"><button data-number="1"class="add-reward-row bg-brand hover:bg-brand-dark text-white text-sm py-1 px-2 ml-2 rounded">리워드추가<i class="xi-plus"></i></button><button data-number="1" class="remove-reward-row bg-gray hover:bg-gray-dark text-white text-sm py-1 px-2 ml-2 rounded">리워드 삭제 <i class="xi-minus"></i></button></div></div><!--reward-fieldset-->';
-
-		var num = $(this).data('number');
-		$('#reward-fieldset-'+num).after(fieldset);
-=======
 	$('.add-reward-row').on('click', function(e){
 		e.preventDefault();
 
@@ -45,39 +33,15 @@ $(function(){
 		fieldset.find('input, select, textarea').val('');
 		$('#reward-fieldset-'+ num).after(fieldset);
 
->>>>>>> project_J
 		setFieldsetNumber();
 	});
 
 	$(document).on('click', '.remove-reward-row', function(e){
-<<<<<<< HEAD
-=======
 
->>>>>>> project_J
 		e.preventDefault();
 
 		var num = $(this).data('number');
 		$('#reward-fieldset-'+num).remove();
-<<<<<<< HEAD
-
-		setFieldsetNumber();
-	});
-
-	$(document).on('click', '.reward-ctrl-btn button', function(){
-		var fieldsetLength = $('.reward-fieldset').length;
-
-		if(fieldsetLength <= 1){
-			console.log($('.remove-reward-row'));
-			var removeBtn = $('.remove-reward-row').detach();
-		}
-		else{
-			$('#reward-fieldset-1 .reward-ctrl-btn').append(removeBtn);
-		}
-	});
-
-	function setFieldsetNumber(){
-		var idx = 1;
-=======
 		setFieldsetNumber();
 		
 	});
@@ -90,7 +54,6 @@ $(function(){
 			console.log($('.remove-reward-row'));
 			$('.remove-reward-row').remove();
 		}
->>>>>>> project_J
 
 		$('.reward-fieldset').each(function(){
 			var fieldset = $(this);
--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -32,43 +32,15 @@ $(function(){
             $(this).parents('.modal').addClass('hidden');
         });
     });
-<<<<<<< HEAD
-<<<<<<< HEAD
-});
-=======
-=======
 
->>>>>>> project_J
 
     $('input.number-only').on('keyup', function(){
         $(this).val($(this).val().replace(/[^0-9]/g,""));
     });
 
     /* 후원 상세페이지 */
-<<<<<<< HEAD
-    $('a.btn-donate').on('click', function(e){
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: $("#donate").offset().top
-        });
-    });
-
-    $('a.add-wishlist').on('click', function(e){
-        e.preventDefault();
-        $(this).toggleClass('added-this');
-    });
-
     $('.campaign-tab-nav .tab-item a').on('click', function(e){
         e.preventDefault();
-        
-        $('html,body').animate({
-            scrollTop: $("#donate").offset().top
-        });
-
-=======
-    $('.campaign-tab-nav .tab-item a').on('click', function(e){
-        e.preventDefault();
->>>>>>> project_J
         $('.campaign-tab-nav .tab-item').removeClass('active');
         $('.campaign-tab-content .tab-content-item').removeClass('active');
 
@@ -81,76 +53,4 @@ $(function(){
         $($(this).attr('href')).toggleClass('hidden');
     });
 
-<<<<<<< HEAD
-    //소셜공유
-    var clipboard = new ClipboardJS('.copy_url');
-	clipboard.on('success', function(e) {
-		alert('URL이 복사되었습니다.');
-	    e.clearSelection();
-	});
-	clipboard.on('error', function() {
-		alert('이 브라우저는 지원하지 않습니다.');
-    });
-    
-    $('.btn-share-sns').on('click', function(){
-        var sns = $(this).data('sns');
-        var url = $(this).data('url');
-        var text = $(this).data('text');
-
-        var w = 720;
-        var h = 350;
-
-        switch(sns){
-            case 'facebook':
-                $.post('https://graph.facebook.com', {id:encodeURI(url),scrape:true});
-                window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(url), 'charitree_share', 'width='+w+',height='+h+',left='+(screen.availWidth-w)*0.5+',top='+(screen.availHeight-h)*0.5);
-                break;
-            case 'twitter':
-                window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(text)+'&url='+encodeURIComponent(url), 'charitree_share', 'width='+w+',height='+h+',left='+(screen.availWidth-w)*0.5+',top='+(screen.availHeight-h)*0.5);
-                break;
-            case 'kakaotalk':
-                var share_img_src = $('meta[property="og:image"]').last().attr('content');
-                if(share_img_src){
-                    Kakao.Link.sendDefault({
-                        objectType: 'feed',
-                        content: {
-                            title: text,
-                            imageUrl: share_img_src,
-                            link: {
-                                mobileWebUrl: url,
-                                webUrl: url
-                            }
-                        }
-                    });
-                }
-                else{
-                    alert('카카오톡 링크에 표시할 이미지를 설정해주세요.');
-                }
-                break;
-        }
-    });
 });
-
-/** 캠페인 상세 폼 유효성 검사 **/
-//코멘트
-function campaign_comment_validation(){
-    if($('textarea[name=campaign_comment]').val().length == 0){
-        alert('내용을 입력해주세요');
-        return false;
-    }
-}
-//문의
-function campaign_qna_validation(){
-    if($('input[name=qna_title]').val().length == 0){
-        alert('제목을 입력해주세요');
-        return false;
-    }
-    if($('textarea[name=qna_question]').val().length == 0){
-        alert('문의내용을 입력해주세요');
-        return false;
-    }
-}
->>>>>>> 31f03afeaac7276b42c0a030c37a732289c38f33
-=======
-});
->>>>>>> project_J
